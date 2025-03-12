e959c210c4922d90157221116f3c9ae695fed1f8
joshuakarthik2005 <joshuakarthik2005@gmail.com>
2025-03-12 21:56:45 +0530

    vercel started

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,23 +1,19 @@
-import { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true,  // Fixes missing pages issue
-  images: {
-    domains: ["https://smartchainerp2.onrender.com/"], // Allow image loading from backend
-  },
+    
 };
 
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/authentication',
-        permanent: true,
-      },
-    ];
-  },
-};
-
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/authentication',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
 export default nextConfig;
