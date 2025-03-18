63855ec7e71d86e6d076aa6cc9e9a04ad9d0378e
joshuakarthik2005 <joshuakarthik2005@gmail.com>
2025-03-18 10:07:18 +0530

    vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,19 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    
+  output: 'export', // ✅ Enables static export mode
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/authentication',
+        permanent: true,
+      },
+    ];
+  },
 };
 
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/authentication',
-          permanent: true,
-        },
-      ];
-    },
-  };
-  
 export default nextConfig;
